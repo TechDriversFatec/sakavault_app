71a7b013db9e920cb255187a95fca2e032c1bc23
davi <davecadastro@gmail.com>
2020-10-04 18:53:41 -0300

    .

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import { isAuthenticated } from "./services/auth";
-
 // Pages import
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import NewSecret from './pages/NewIncident';
-
-// const PrivateRoute = ({ component: Component, ...rest }) => (
-//     <Route
-//       {...rest}
-//       render={props =>
-//         isAuthenticated() ? (
-//           <Component {...props} />
-//         ) : (
-//           <Redirect to={{ pathname: "/", state: { from: props.location } }} />
-//         )
-//       }
-//     />
-// );
 
 const Routes = () => (
     <BrowserRouter>
