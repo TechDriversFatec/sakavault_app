e4bfd9c3e27c6992aa71b0c4467468d66d86be6f
davi <davecadastro@gmail.com>
2020-10-07 11:06:28 -0300

    minor update

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -35,7 +35,6 @@ export default function Profile() {
     useEffect( () => {
         if(userToken == null){ 
             history.push('/');
-            return;
         }else{
             api.get('secrets', {
                 headers: {
